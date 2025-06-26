f6243a4c56b62164546b46dded9fe26aa92eb416
Ashish Patel <apatel2@ualberta.ca>
2025-06-26 05:45:49 -0600

    add lib/types.ts

---
+++ lib/types.ts
@@ -0,0 +1,36 @@
+export interface TimeSlot {
+  day: string
+  startTime: string
+}
+
+export interface AvailabilitySlot {
+  start: string
+  end: string
+}
+
+export interface Candidate {
+  id: string
+  name: string
+  availability: {
+    [day: string]: AvailabilitySlot[]
+  }
+}
+
+export interface Engineer {
+  id: string
+  name: string
+  role: string
+  availability: {
+    [day: string]: AvailabilitySlot[]
+  }
+}
+
+export interface BookedSlot {
+  id: string
+  candidateId: string
+  engineerId: string
+  day: string
+  startTime: string
+  duration: number
+  bookedAt: Date
+}
